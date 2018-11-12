198844dd751ab964dd2d991a92c734ccbc7c56f7
ladygenesis28 <ladygenesis28@yahoo.com>
2018-11-12 12:16:01 -0500

    2nd  commit

--- app/src/main/java/com/example/ladyg/newleafchildrenbookstoreinventoryapp1/CatalogActivity.java
+++ app/src/main/java/com/example/ladyg/newleafchildrenbookstoreinventoryapp1/CatalogActivity.java
@@ -26,7 +26,7 @@ public class CatalogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_catalog);
 
         // Setup FAB to open EditorActivity
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = TextView (findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
